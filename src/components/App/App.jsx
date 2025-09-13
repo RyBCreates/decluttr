8e97564659f019888144de8593f887fb4cf3d653
Ryan B <bombingrun3@gmail.com>
2025-09-13 01:30:40 -0400

    Added Basic layout of Shop page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import SideBar from "../LeftSideBar/LeftSideBar";
 import Home from "../pages/Home/Home";
 import Profile from "../pages/Profile/Profile";
+import Shop from "../pages/Shop/Shop";
 
 import "./App.css";
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="profile" element={<Profile />} />
+          <Route path="shop" element={<Shop />} />
         </Routes>
       </div>
     </div>
