37c6ddbf35d2af0bb3e028e1f5c94d964ef30609
Jayden King <itsjaydenking@gmail.com>
2025-09-14 11:10:30 -0700

    Refactor RightSideBar component and update icon assets for improved UI

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,19 +1,30 @@
 import "./RightSideBar.css";
+import fire from "../../assets/icons/fire-icon.svg";
+import points from "../../assets/icons/points-icon.svg";
+import experience from "../../assets/icons/exp-icon.svg";
 
 function RightSideBar() {
   return (
     <div className="right-sidebar">
-      <ul className="right-sidebar__stats">
-        <li className="right-sidebar__stat">
-          <p className="right-sidebar__streak">🔥 364</p>
-        </li>
-        <li className="right-sidebar__stat">
-          <p className="right-sidebar__gems">💎 999</p>
-        </li>
-        <li className="right-sidebar__stat">
-          <p className="right-sidebar__experience">🌟 10</p>
-        </li>
-      </ul>
+      <div className="right-sidebar__stat">
+        <p className="right-sidebar__label">Daily Streak</p>
+        <p className="right-sidebar__value">
+          <img src={fire} alt="" className="right-sidebar__icon" />
+          364
+        </p>
+      </div>
+      <div className="right-sidebar__stat">
+        <p className="right-sidebar__label">Points</p>
+        <p className="right-sidebar__value">
+          <img src={points} alt="" className="right-sidebar__icon" /> 999
+        </p>
+      </div>
+      <div className="right-sidebar__stat">
+        <p className="right-sidebar__label">Level</p>
+        <p className="right-sidebar__value">
+          <img src={experience} alt="" className="right-sidebar__icon" /> 10
+        </p>
+      </div>
     </div>
   );
 }
