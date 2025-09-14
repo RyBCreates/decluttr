953cca251f477808aa22ce9019408ccd795425dc
Jayden King <itsjaydenking@gmail.com>
2025-09-14 11:14:47 -0700

    Refactor RightSideBar to use user context for dynamic stats display

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -2,27 +2,36 @@ import "./RightSideBar.css";
 import fire from "../../assets/icons/fire-icon.svg";
 import points from "../../assets/icons/points-icon.svg";
 import experience from "../../assets/icons/exp-icon.svg";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 
 function RightSideBar() {
+  const user = useContext(UserContext);
+
+  const streak = user?.streak ?? 0;
+  const gems = user?.gems ?? 0;
+  const level = user?.level ?? 0;
+
   return (
     <div className="right-sidebar">
       <div className="right-sidebar__stat">
         <p className="right-sidebar__label">Daily Streak</p>
         <p className="right-sidebar__value">
           <img src={fire} alt="" className="right-sidebar__icon" />
-          364
+          {streak}
         </p>
       </div>
       <div className="right-sidebar__stat">
         <p className="right-sidebar__label">Points</p>
         <p className="right-sidebar__value">
-          <img src={points} alt="" className="right-sidebar__icon" /> 999
+          <img src={points} alt="" className="right-sidebar__icon" /> {gems}
         </p>
       </div>
       <div className="right-sidebar__stat">
         <p className="right-sidebar__label">Level</p>
         <p className="right-sidebar__value">
-          <img src={experience} alt="" className="right-sidebar__icon" /> 10
+          <img src={experience} alt="" className="right-sidebar__icon" />{" "}
+          {level}
         </p>
       </div>
     </div>
