4c4be7e7f9001454682be6b4517e446bab3cfa95
Ryan B <bombingrun3@gmail.com>
2025-09-16 00:06:33 -0400

    moved some assets to public folder, updated mockData structure

--- src/utils/mockData/mockTasks.js
+++ src/utils/mockData/mockTasks.js
@@ -15,3 +15,142 @@ export const tasks = [
   { id: 14, name: "Clean Microwave", gems: 10, icon: "🧼" },
   { id: 15, name: "Take out Recycling", gems: 5, icon: "♻️" },
 ];
+
+// Update Frontend to accept this data structure(reward structure changed)
+// export const tasks = [
+//   {
+//     id: 1,
+//     name: "Clean Desk",
+//     reward: {
+//       gems: 10,
+//       xp: 10,
+//     },
+//     icon: "🧹",
+//   },
+//   {
+//     id: 2,
+//     name: "Clean Park",
+//     reward: {
+//       gems: 25,
+//       xp: 10,
+//     },
+//     icon: "🏞️",
+//   },
+//   {
+//     id: 3,
+//     name: "Laundry",
+//     reward: {
+//       gems: 10,
+//       xp: 10,
+//     },
+//     icon: "🧺",
+//   },
+//   {
+//     id: 4,
+//     name: "Clean Bathroom",
+//     reward: {
+//       gems: 15,
+//       xp: 10,
+//     },
+//     icon: "🧴",
+//   },
+//   {
+//     id: 5,
+//     name: "Clean Kitchen",
+//     reward: {
+//       gems: 20,
+//       xp: 10,
+//     },
+//     icon: "🍽️",
+//   },
+//   {
+//     id: 6,
+//     name: "Take out Trash",
+//     reward: {
+//       gems: 15,
+//       xp: 10,
+//     },
+//     icon: "🗑️",
+//   },
+//   {
+//     id: 7,
+//     name: "Mop Floors",
+//     reward: {
+//       gems: 10,
+//       xp: 10,
+//     },
+//     icon: "🧹",
+//   },
+//   {
+//     id: 8,
+//     name: "Dust Surfaces",
+//     reward: {
+//       gems: 20,
+//       xp: 10,
+//     },
+//     icon: "🧽",
+//   },
+//   {
+//     id: 9,
+//     name: "Wipe Windows",
+//     reward: {
+//       gems: 20,
+//       xp: 10,
+//     },
+//     icon: "🧽",
+//   },
+//   {
+//     id: 10,
+//     name: "Clean Fridge",
+//     reward: {
+//       gems: 25,
+//       xp: 10,
+//     },
+//     icon: "🧊",
+//   },
+//   {
+//     id: 11,
+//     name: "Organize Closet",
+//     reward: {
+//       gems: 20,
+//       xp: 10,
+//     },
+//     icon: "👚",
+//   },
+//   {
+//     id: 12,
+//     name: "Sweep Porch",
+//     reward: {
+//       gems: 10,
+//       xp: 10,
+//     },
+//     icon: "🧹",
+//   },
+//   {
+//     id: 13,
+//     name: "Change Bed Sheets",
+//     reward: {
+//       gems: 10,
+//       xp: 10,
+//     },
+//     icon: "🛏️",
+//   },
+//   {
+//     id: 14,
+//     name: "Clean Microwave",
+//     reward: {
+//       gems: 10,
+//       xp: 10,
+//     },
+//     icon: "🧼",
+//   },
+//   {
+//     id: 15,
+//     name: "Take out Recycling",
+//     reward: {
+//       gems: 5,
+//       xp: 10,
+//     },
+//     icon: "♻️",
+//   },
+// ];
