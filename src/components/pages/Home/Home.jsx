3cb5cfd5178c714680eb7b31eb587a0cd261a7de
Ryan B <bombingrun3@gmail.com>
2025-09-16 00:39:11 -0400

    Connected Tasks Get function from backend

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,18 +1,26 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { CurrentUserContext } from "../../../contexts/UserContext";
 
 import RightSideBar from "../../RightSideBar/RightSideBar";
 import TaskCard from "../../TaskCard/TaskCard";
-import { tasks as mockTasks } from "../../../utils/mockData/mockTasks";
+import { getTasks } from "../../../utils/api/tasks";
 
 import "./Home.css";
 
-function Home({ achievementVariant }) {
-  const [tasks, setTasks] = useState([...mockTasks]);
+function Home() {
+  const [tasks, setTasks] = useState([]);
   const { user, setUser } = useContext(CurrentUserContext);
 
+  useEffect(() => {
+    async function loadTasks() {
+      const data = await getTasks();
+      setTasks(data);
+    }
+    loadTasks();
+  }, []);
+
   const deleteTask = (id) => {
-    setTasks((prev) => prev.filter((task) => task.id !== id));
+    setTasks((prev) => prev.filter((task) => task._id !== id));
   };
 
   const toggleTask = (id, gems) => {
@@ -60,16 +68,27 @@ function Home({ achievementVariant }) {
         <h1 className="home__title">Daily Tasks</h1>
         <p className="home__add-task">+ Add Task</p>
         <div className="home__task-gallery">
-          {tasks.map((task) => (
-            <TaskCard
-              key={task.id}
-              icon={task.icon}
-              name={task.name}
-              gems={task.gems}
-              onDelete={() => deleteTask(task.id)}
-              onToggle={() => toggleTask(task.id, task.gems)}
-            />
-          ))}
+          {tasks.length === 0 ? (
+            <>
+              <p className="home__tasks-empty">
+                Looks like you are out of tasks!
+              </p>
+              <button className="home__add-task_secondary" type="button">
+                Add Task
+              </button>
+            </>
+          ) : (
+            tasks.map((task) => (
+              <TaskCard
+                key={task._id}
+                icon={task.icon}
+                name={task.name}
+                gems={task.reward.gems}
+                onDelete={() => deleteTask(task._id)}
+                onToggle={() => toggleTask(task._id, task.reward.gems)}
+              />
+            ))
+          )}
         </div>
       </div>
 
